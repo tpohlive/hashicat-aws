ff9550b8ce272a61704515df49b06deadfca4c85
Christian De Leon <miapaintball1@gmail.com>
2022-12-12 19:45:23 +0000

    Fix effor with s3 module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/chrisitan-org/s3-bucket/aws"
+  version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = var.prefix
-
-  versioning = {
-    enabled = true
-  }
-
 }
